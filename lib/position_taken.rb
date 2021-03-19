f1eca29bfd5023ba60268d8c582fcd7d716b059b
Michael Raysor <michaelraysor1@gmail.com>
2021-03-19 00:32:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 
 def position_taken? (board,index)
 if board[index] == "" || board[index] == " " || board[index] == nil
-  return false
+   false
 elsif board[index] == "X" || board[index] == "O"
-  return true
+   true
 end
 end
